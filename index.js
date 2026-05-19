4803ba2044aa57f92a05c5694d1a23f39084d06b
Ayush Sharma <ayush.sharma170907@gmail.com>
2026-05-19 12:13:50 +0530

    adding login and singup page

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const ejsMate = require("ejs-mate");
 const expressError = require("./utils/expressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 main()
   .then((res) => {
@@ -47,6 +51,13 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -54,10 +65,13 @@ app.use((req, res, next) => {
 });
 
 // Listing route
-app.use("/listings", listings);
+app.use("/listings", listingRouter);
 
 // Reviews Route
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings/:id/reviews", reviewsRouter);
+
+// User Route(singup or login)
+app.use("/", userRouter);
 
 // Error Route for page not found
 app.use((req, res, next) => {
@@ -73,5 +87,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Service is listning at port ${port}`);
+  console.log(`Server is listning at port ${port}`);
 });
